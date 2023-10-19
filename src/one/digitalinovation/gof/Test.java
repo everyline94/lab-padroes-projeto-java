8222cee8f36601b624fda7f982a47d4ecc9c7de7
bruno larocca <brunolujanlarocca@gmail.com>
2023-10-19 15:24:58 -0300

    ok

---
+++ src/one/digitalinovation/gof/Test.java
@@ -0,0 +1,44 @@
+package one.digitalinovation.gof;
+
+import one.digitalinovation.gof.facade.Facade;
+import one.digitalinovation.gof.singleton.SingletonEager;
+import one.digitalinovation.gof.singleton.SingletonLazy;
+import one.digitalinovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinovation.gof.strategy.*;
+
+public class Test {
+    public static void main(String[] args) {
+
+        //Testes relacionados ao Design Pattern Singleton:
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+
+        //Strategy
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.mover();
+
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+
+        //Facade
+        Facade facade = new Facade();
+        facade.migrarCliente("Bruno", "14801308");
+    }
+}
